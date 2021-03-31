e59dc82673d0a9d7b6b439738669e9cb73d65bf7
thetaepsilon <thetaepsilon@noreply.users.github.com>
2021-03-31 14:07:32 +0100

    [-] src/spp/nudge/5x5_lcd_u_centred.frag.glsl: missing semicolon

--- src/spp/nudge/5x5_lcd_u_centred.frag.glsl
+++ src/spp/nudge/5x5_lcd_u_centred.frag.glsl
@@ -5,7 +5,7 @@
 
 
 #define USE_MODULATION_LUT 1
-const float D1 = ${d1:0.5}
+const float D1 = ${d1:0.5};
 #define ROW(v)	vec3(0), vec3(v*D1), vec3(v), vec3(v*D1), vec3(0)
 const vec3 modulate_lut[5*5] = vec3[](
 	ROW(1),
