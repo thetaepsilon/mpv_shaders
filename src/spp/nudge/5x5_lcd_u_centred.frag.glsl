7ed0b1394a28f015032b07b907c12893f4d28f1c
thetaepsilon <thetaepsilon@noreply.users.github.com>
2021-03-31 14:04:59 +0100

    [+] src/spp/nudge: centered 5x5 variant of lcd dark grid effect

---
+++ src/spp/nudge/5x5_lcd_u_centred.frag.glsl
@@ -0,0 +1,20 @@
+//!WIDTH HOOKED.w 5 *
+//!HEIGHT HOOKED.h 5 *
+#define HPIXSZ 5
+#define VPIXSZ 5
+
+
+#define USE_MODULATION_LUT 1
+const float D1 = ${d1:0.5}
+#define ROW(v)	vec3(0), vec3(v*D1), vec3(v), vec3(v*D1), vec3(0)
+const vec3 modulate_lut[5*5] = vec3[](
+	ROW(1),
+	ROW(1),
+	ROW(1),
+	ROW(1),
+	ROW(0)
+);
+#undef ROW
+
+#define MODULATE_BASE_COLOR ${basecol:0, 0, 0}
+
