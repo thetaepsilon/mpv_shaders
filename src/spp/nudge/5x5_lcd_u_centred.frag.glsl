e9ed68818dee8c3fc01e9305c648973bcba73c66
thetaepsilon <thetaepsilon@noreply.users.github.com>
2021-03-31 16:07:35 +0100

    [+] 5x5_lcd_u_centred.frag.glsl: allow adjusting edge column intensity

--- src/spp/nudge/5x5_lcd_u_centred.frag.glsl
+++ src/spp/nudge/5x5_lcd_u_centred.frag.glsl
@@ -6,7 +6,8 @@
 
 #define USE_MODULATION_LUT 1
 const float D1 = ${d1:0.5};
-#define ROW(v)	vec3(0), vec3(v*D1), vec3(v), vec3(v*D1), vec3(0)
+const float D2 = ${d2:0.};
+#define ROW(v)	vec3(v*D2), vec3(v*D1), vec3(v), vec3(v*D1), vec3(v*D2)
 const vec3 modulate_lut[5*5] = vec3[](
 	ROW(1),
 	ROW(1),
