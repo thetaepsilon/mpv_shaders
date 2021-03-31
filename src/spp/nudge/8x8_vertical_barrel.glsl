ce9eb80f4e52d1cfe424398416af5087760292d8
thetaepsilon <thetaepsilon@noreply.users.github.com>
2021-03-31 17:50:05 +0100

    [+] spp/nudge: 8x8 soft barrel tile effect

---
+++ src/spp/nudge/8x8_vertical_barrel.glsl
@@ -0,0 +1,35 @@
+//!WIDTH HOOKED.w 8 *
+//!HEIGHT HOOKED.h 8 *
+#define HPIXSZ 8
+#define VPIXSZ 8
+
+
+#define USE_MODULATION_LUT 1
+const float D1 = ${d1:0.9};
+const float D2 = ${d2:0.6561};
+const float D3 = ${d2:0.};
+const vec3 Z = vec3(0.);
+#define ROW(v) \
+	vec3(v*D3), \
+	vec3(v*D2), \
+	vec3(v*D1), \
+	vec3(v), \
+	vec3(v), \
+	vec3(v*D1), \
+	vec3(v*D2), \
+	vec3(v*D3)
+
+const vec3 modulate_lut[8*8] = vec3[](
+	ROW(1.),
+	ROW(1.),
+	ROW(1.),
+	ROW(1.),
+	ROW(1.),
+	ROW(1.),
+	ROW(1.),
+	ROW(0.)
+);
+#undef ROW
+
+#define MODULATE_BASE_COLOR ${basecol:0, 0, 0}
+
