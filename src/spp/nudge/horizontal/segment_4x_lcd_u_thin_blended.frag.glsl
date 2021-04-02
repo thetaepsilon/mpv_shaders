cc2f18bec01c32968a74d8fe492b8c261e5fc9ed
thetaepsilon <thetaepsilon@noreply.users.github.com>
2021-04-02 16:05:43 +0100

    [+] spp/nudge/horizontal: 4x lcd thin blended segment

---
+++ src/spp/nudge/horizontal/segment_4x_lcd_u_thin_blended.frag.glsl
@@ -0,0 +1,27 @@
+//!WIDTH HOOKED.w 4 *
+//!HEIGHT HOOKED.h 1 *
+#define HPIXSZ 4
+#define VPIXSZ 1
+
+
+#define USE_DRIFT_LUT 1
+#define DRIFT_LUT_IS_ABS 1
+#define V(x) vec2(x, 0.)
+const vec2 drift_lut[4] = vec2[](
+	V(-0.33333),
+	V(0.00),
+	V(0.33333),
+	V(0.66666)
+);
+
+
+#define USE_MODULATION_LUT 1
+const float D1 = float(${d1:(9. / 16.)});
+const float D2 = float(${d2:D1});
+const vec3 modulate_lut[4*1] = vec3[](
+	vec3(D1),
+	vec3(1.),
+	vec3(D1),
+	vec3(D2)
+);
+
