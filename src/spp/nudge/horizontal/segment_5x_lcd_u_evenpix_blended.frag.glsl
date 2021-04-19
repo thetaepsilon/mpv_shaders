ae54726c7cf7dc14633ebdc94bd6bc89905ee49e
thetaepsilon <thetaepsilon@noreply.users.github.com>
2021-04-19 14:31:19 +0100

    [+] spp/nudge/horiontal: even-pixel-width segment variants for 4pix and 5pix LCD u segments

---
+++ src/spp/nudge/horizontal/segment_5x_lcd_u_evenpix_blended.frag.glsl
@@ -0,0 +1,31 @@
+//!WIDTH HOOKED.w 5 *
+//!HEIGHT HOOKED.h 1 *
+#define HPIXSZ 5
+#define VPIXSZ 1
+
+
+#define USE_DRIFT_LUT 1
+#define DRIFT_LUT_IS_ABS 1
+#define V(x) vec2(x, 0.)
+const float N1 = float(${n1:0.25});
+const float N2 = 0.5;
+const vec2 drift_lut[5] = vec2[](
+	V(-N2),
+	V(-N1),
+	V(0.0),
+	V(0.0),
+	V(N1)
+);
+
+
+#define USE_MODULATION_LUT 1
+const float D1 = float(${d1});
+const float D2 = float(${d2:D1});
+const vec3 modulate_lut[5*1] = vec3[](
+	vec3(D2),
+	vec3(D1),
+	vec3(1.),
+	vec3(1.),
+	vec3(D1)
+);
+
