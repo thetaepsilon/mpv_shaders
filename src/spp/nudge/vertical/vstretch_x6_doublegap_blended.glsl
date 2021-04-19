a174d9114c7dbdab4f6272a5d168e5e522de91ee
thetaepsilon <thetaepsilon@noreply.users.github.com>
2021-04-19 14:36:18 +0100

    [+] spp/nudge: add 1D vertical subdir with an asymmetric, double-pixel-gap version of vstretch_x6

---
+++ src/spp/nudge/vertical/vstretch_x6_doublegap_blended.glsl
@@ -0,0 +1,35 @@
+//!WIDTH HOOKED.w 1 *
+//!HEIGHT HOOKED.h 6 *
+#define HPIXSZ 1
+#define VPIXSZ 6
+
+
+#define USE_DRIFT_LUT 1
+#define DRIFT_LUT_IS_ABS 1
+#define Z vec2(0.)
+const float N1 = float(${n1:0.33});
+const float UN1 = float(${un1:-N1});
+vec2 drift_lut[] = vec2[](
+	vec2(0., UN1),
+	Z,
+	Z,
+	Z,
+	Z,
+	vec2(0., N1)
+);
+#undef Z
+
+#define USE_MODULATION_LUT 1
+#define W vec3(1.)
+const float D1 = float(${d1:0.5});
+const float UD1 = float(${ud1:D1});
+const vec3 modulate_lut[] = vec3[](
+	vec3(UD1),
+	W,
+	W,
+	W,
+	W,
+	vec3(D1)
+);
+#undef W
+
