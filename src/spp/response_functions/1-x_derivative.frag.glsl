00342c9d6f98e9f7d43bdafb48efe87dbb5be480
thetaepsilon <thetaepsilon@noreply.users.github.com>
2021-06-14 17:14:17 +0100

    [*] spp/response functions: comment-disable _MAX constants as they are not currently used anywhere.

--- src/spp/response_functions/1-x_derivative.frag.glsl
+++ src/spp/response_functions/1-x_derivative.frag.glsl
@@ -1,12 +1,12 @@
 #define USE_RESPONSE_FUNCTIONS 1
 #define RESPONSE_FUNCTIONS_EXPECT_CLAMPED 1
 
-#define RESPONSE_FUNCTION_WELL_MAX 1.0
+//#define RESPONSE_FUNCTION_WELL_MAX 1.0
 vec3 well_response(vec3 c) {
 	return (vec3(2.) * c) - (c * c);
 }
 
-#define RESPONSE_FUNCTION_LEAK_MAX 1.0
+//#define RESPONSE_FUNCTION_LEAK_MAX 1.0
 vec3 leak_response(vec3 c) {
 	return c * c;
 }
