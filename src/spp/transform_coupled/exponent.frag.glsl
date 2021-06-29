27ebc32317b6368212436d37064876c0a3b7d4ee
thetaepsilon <thetaepsilon@noreply.users.github.com>
2021-06-29 20:48:44 +0100

    [-] spp/transform_coupled: exponent can't handle negative inputs, fix with min clamp

--- src/spp/transform_coupled/exponent.frag.glsl
+++ src/spp/transform_coupled/exponent.frag.glsl
@@ -2,9 +2,11 @@ const vec3 exponent = vec3(${e});
 const vec3 exponent_inverse = vec3(1.) / exponent;
 
 vec3 f(vec3 c) {
+	c = max(c, vec3(0.0));
 	return pow(c, exponent);
 }
 vec3 f_undo(vec3 c) {
+	c = max(c, vec3(0.0));
 	return pow(c, exponent_inverse);
 }
 
