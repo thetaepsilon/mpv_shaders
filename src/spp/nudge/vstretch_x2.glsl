f1a2df7478197f844f40afa16dee6a487f069b6a
thetaepsilon <thetaepsilon@noreply.users.github.com>
2021-07-25 12:21:27 +0100

    [+] spp/nudge: 2x vstrech, aka poor man's scanlines

---
+++ src/spp/nudge/vstretch_x2.glsl
@@ -0,0 +1,22 @@
+//!WIDTH HOOKED.w 1 *
+//!HEIGHT HOOKED.h 2 *
+#define HPIXSZ 1
+#define VPIXSZ 2
+
+
+#define USE_DRIFT_LUT 1
+#define DRIFT_LUT_IS_ABS 1
+#define Z vec2(0.)
+vec2 drift_lut[] = vec2[](
+	Z,
+	vec2(0., 0.5)
+);
+#undef Z
+
+#define USE_MODULATION_LUT 1
+const vec3 modulate_lut[] = vec3[](
+	vec3(1.),
+	vec3(${vstretch_rowdim:0.})
+);
+#undef W
+
