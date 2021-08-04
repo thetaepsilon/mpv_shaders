b99dc6cbf1fb977c7d4e64360faf5b2ab80c3274
thetaepsilon <thetaepsilon@noreply.users.github.com>
2021-08-04 13:45:57 +0100

    [+] bloom_extdata.svar.glsl: input_gamma and disable_clamp

--- src/bloom_extdata.svar.glsl
+++ src/bloom_extdata.svar.glsl
@@ -16,13 +16,18 @@ vec4 hook() {
 	vec3 reference = TEXF(p).rgb;
 	vec3 addend = BTEXF(p).rgb * addm;
 
+//#optreplace reference = pow(reference, vec3(${input_gamma}) );
+//#optreplace addend = pow(addend, vec3(${input_gamma}) );
+
 	//bool ab = fract(frame / 2.0) < 0.5;
 	vec3 result = (reference + addend);
 
 	result *= exposure;
 
 	// ensure the result doesn't blare out - some shader passes don't like this
+//#optreplace #if 0	// ${disable_clamp}
 	result = min(result, vec3(1.0));
+//#optreplace #endif	// ${disable_clamp}
 
 //#optreplace result.rgb = pow(result.rgb, vec3(1 / ${output_gamma}));
 	return vec4(result, 1.);
