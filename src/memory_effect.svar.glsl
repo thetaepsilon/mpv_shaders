6ca7890e9f84471c5e83499700448f297ea932b3
thetaepsilon <thetaepsilon@noreply.users.github.com>
2021-08-11 18:50:36 +0100

    [+] clean up memory effect / frame interpolation shader and promote out of old shaders

--- src/memory_effect.svar.glsl
+++ src/memory_effect.svar.glsl
@@ -1,29 +1,32 @@
-//!HOOK MAINPRESUB
-//!BIND MAINPRESUB
+//!HOOK ${at:MAINPRESUB}
+//!BIND ${in}
 //!DESC memory effect buffer 1
-//!WIDTH MAINPRESUB.w
-//!HEIGHT MAINPRESUB.h
+//!WIDTH ${in}.w
+//!HEIGHT ${in}.h
 //!SAVE flipbuf1
 vec4 hook() {
-	if (mod(frame, 2) >= 1) discard;
-	return MAINPRESUB_tex(MAINPRESUB_pos);
+	if (mod(float(frame), 2.0) >= 1.0) discard;
+	return ${in}_tex(${in}_pos);
 }
 
-//!HOOK MAINPRESUB
-//!BIND MAINPRESUB
+//!HOOK ${at:MAINPRESUB}
+//!BIND ${in}
 //!DESC memory effect buffer 2
-//!WIDTH MAINPRESUB.w
-//!HEIGHT MAINPRESUB.h
+//!WIDTH ${in}.w
+//!HEIGHT ${in}.h
 //!SAVE flipbuf2
 vec4 hook() {
-	if (mod(frame, 2) < 1) discard;
-	return MAINPRESUB_tex(MAINPRESUB_pos);
+	if (mod(float(frame), 2.0) < 1.0) discard;
+	return ${in}_tex(${in}_pos);
 }
 
-//!HOOK MAINPRESUB
+//!HOOK ${at:MAINPRESUB}
 //!BIND flipbuf1
 //!BIND flipbuf2
 //!DESC memory effect overwrite pass
+//!WIDTH ${in}.w
+//!HEIGHT ${in}.h
+//!SAVE ${in}
 const float ghosting_strength = 0.5;
 vec4 hook() {
 	vec2 pos = flipbuf1_pos;
@@ -36,6 +39,6 @@ vec4 hook() {
 	// (ow, that sentence even makes _my_ head hurt >_>).
 	// the "losing" buffer gets down-weighted to 0.25 of the total MAINPRESUB.
 	// note it's seemingly backwards below as in the mix(), 0.0 means all c1.
-	float pick = (mod(frame, 2) == 0) ? ghosting_strength : (1. - ghosting_strength);
+	float pick = ( int(mod(float(frame), 2.0)) == 0) ? ghosting_strength : (1. - ghosting_strength);
 	return mix(c1, c2, pick);
 }
