61b3c36eba35b83ef84f7e2659f9f2591f19aa1d
thetaepsilon <thetaepsilon@noreply.users.github.com>
2021-08-12 20:55:56 +0100

    [+] explicit alternate buffer variant of exact output pass (preserves svar-less exact_output compat with existing known chains)

---
+++ src/exact_output_altbuf.svar.glsl
@@ -0,0 +1,22 @@
+//!HOOK POSTKERNEL
+//!DESC pixel exact display pass (no scaling), custom input source
+//!BIND ${in}
+#define TEXF ${in}_tex
+#define SZ ${in}_size
+vec2 pad = vec2(0, 0);
+bool outofbounds(float f) {
+	return (f < 0.) || (f > 1.);
+}
+vec4 hook() {
+	vec2 pix = gl_FragCoord.xy - pad;
+	vec2 pos = pix / SZ;
+	bool nowrite = (outofbounds(pos.x) || outofbounds(pos.y));
+	if (nowrite) {
+		if (mod(float(frame), 60.) < 1.) {
+			return vec4(0.);
+		} else {
+			discard;
+		}
+	}
+	return TEXF(pos);
+}
