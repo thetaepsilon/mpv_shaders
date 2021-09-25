865f4703d7ff487696a3a232a338215d2afcb543
thetaepsilon <thetaepsilon@noreply.users.github.com>
2021-09-25 11:24:16 +0100

    [-] bar_filter_horizontal.svar.glsl: GLSL ES type conversion fixes

--- src/bar_filter_horizontal.svar.glsl
+++ src/bar_filter_horizontal.svar.glsl
@@ -12,7 +12,7 @@
 const int distance_r = ${r};
 const int distance_l = ${l};
 const int dia = distance_r + distance_l + 1;
-const float scaler = 1 / float(dia) * ${e:1.};
+const float scaler = 1.0 / float(dia) * ${e:1.};
 
 vec4 hook() {
 	float ox = gl_FragCoord.x;
@@ -20,7 +20,7 @@ vec4 hook() {
 
 	vec4 total = vec4(0.);
 	for (int x = -distance_l; x <= distance_r; x++) {
-		vec2 srcpix = vec2(ox + x, oy);
+		vec2 srcpix = vec2(ox + float(x), oy);
 		vec2 srcpt = srcpix / SZ;
 		vec4 data = TEXF(srcpt);
 		total += data;
