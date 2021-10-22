c4e07534ee1ba1062a930bf60416e923b9867720
thetaepsilon <thetaepsilon@noreply.users.github.com>
2021-10-22 15:49:01 +0100

    [-] bar_filter_horizontal.svar.glsl: proper packed vector math and GLSL ES type conversion fix

--- src/bar_filter_horizontal.svar.glsl
+++ src/bar_filter_horizontal.svar.glsl
@@ -15,12 +15,11 @@ const int dia = distance_r + distance_l + 1;
 const float scaler = 1.0 / float(dia) * ${e:1.};
 
 vec4 hook() {
-	float ox = gl_FragCoord.x;
-	float oy = gl_FragCoord.y;
+	vec2 origin = gl_FragCoord.xy;
 
 	vec4 total = vec4(0.);
 	for (int x = -distance_l; x <= distance_r; x++) {
-		vec2 srcpix = vec2(ox + float(x), oy);
+		vec2 srcpix = origin + vec2(float(x), 0.);
 		vec2 srcpt = srcpix / SZ;
 		vec4 data = TEXF(srcpt);
 		total += data;
