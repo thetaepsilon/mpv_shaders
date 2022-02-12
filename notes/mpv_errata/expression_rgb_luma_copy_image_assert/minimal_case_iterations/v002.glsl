7f3be7f9e2ec76f11fbfdc1da2b86fa7e8a1b54f
thetaepsilon <thetaepsilon@noreply.users.github.com>
2022-02-12 14:12:41 +0000

    [+] mpv_errata: expression_rgb_luma_copy_image_assert: iteration v002, remove DESC directive

---
+++ notes/mpv_errata/expression_rgb_luma_copy_image_assert/minimal_case_iterations/v002.glsl
@@ -0,0 +1,12 @@
+//!HOOK LUMA
+//!BIND LUMA
+//!SAVE LUMA
+//!WIDTH LUMA.w
+//!HEIGHT LUMA.h
+//!COMPONENTS 3
+
+vec4 hook() {
+	vec3 c = LUMA_tex(LUMA_pos).rgb;
+	c = vec3(0.5);
+	return vec4(c, 1.);
+}
