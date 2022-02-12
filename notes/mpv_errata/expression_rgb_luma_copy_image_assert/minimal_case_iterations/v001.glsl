3915ea28dfae75bce7f3ec56fc2dc410fbbac1ae
thetaepsilon <thetaepsilon@noreply.users.github.com>
2022-02-12 14:10:45 +0000

    [+] mpv_errata: expression_rgb_luma_copy_image_assert: iteration v001, formatting

---
+++ notes/mpv_errata/expression_rgb_luma_copy_image_assert/minimal_case_iterations/v001.glsl
@@ -0,0 +1,13 @@
+//!HOOK LUMA
+//!DESC transform rgb according to arbitrary expression
+//!BIND LUMA
+//!SAVE LUMA
+//!WIDTH LUMA.w
+//!HEIGHT LUMA.h
+//!COMPONENTS 3
+
+vec4 hook() {
+	vec3 c = LUMA_tex(LUMA_pos).rgb;
+	c = vec3(0.5);
+	return vec4(c, 1.);
+}
