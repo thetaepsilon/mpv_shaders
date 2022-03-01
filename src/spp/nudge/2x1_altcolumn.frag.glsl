aaffd2ff55959a718f5e505baea02a9819d3713b
thetaepsilon <thetaepsilon@noreply.users.github.com>
2022-03-01 16:53:01 +0000

    [+] spp/nudge: 2x1 pixelated upscaler with alternate black columns

---
+++ src/spp/nudge/2x1_altcolumn.frag.glsl
@@ -0,0 +1,13 @@
+//!WIDTH ${in}.w 2 *
+//!HEIGHT ${in}.h
+#define HPIXSZ 2
+#define VPIXSZ 1
+
+
+
+#define USE_MODULATION_LUT 1
+const vec3 modulate_lut[2*1] = vec3[](
+	vec3(1.0),
+	vec3(0.0)
+);
+
