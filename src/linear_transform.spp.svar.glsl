2818196376b2cd435e419de725828709dccb77a2
thetaepsilon <thetaepsilon@noreply.users.github.com>
2022-03-05 14:31:15 +0000

    [+] linear_transform.spp.svar.glsl: allow overriding transform expression from f(c), make external function include optional

--- src/linear_transform.spp.svar.glsl
+++ src/linear_transform.spp.svar.glsl
@@ -6,12 +6,12 @@
 //!HEIGHT ${in}.h
 //!COMPONENTS 3
 
-//#include function
+//#include? function
 
 vec4 hook() {
 	vec3 c = ${in}_tex(${in}_pos).rgb;
 	bool cond = (${ab_expr:true});
-	if (cond) c = f(c);
+	if (cond) c = ${expr:f(c)};
 	return vec4(c, 1.);
 }
 
