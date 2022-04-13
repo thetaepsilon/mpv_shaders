a3d662a6fbc487c6a7c704e71298b496a49b0209
thetaepsilon <thetaepsilon@noreply.users.github.com>
2022-04-13 15:57:21 +0100

    [+] blur kernels: seperable binomial gaussian approximations

---
+++ src/spp/blur_kernels/separable/binomial/binomial_11x.frag.glsl
@@ -0,0 +1,19 @@
+const float kernel_data[] = float[](
+
+1.,
+10.,
+45.,
+120.,
+210.,
+252.,
+210.,
+120.,
+45.,
+10.,
+1.
+
+);
+
+const int kernel_radius = 5;
+#define KERNEL_SCALE_VALUE 1./1024.
+
---
+++ src/spp/blur_kernels/separable/binomial/binomial_15x.frag.glsl
@@ -0,0 +1,23 @@
+const float kernel_data[] = float[](
+
+1.,
+14.,
+91.,
+364.,
+1001.,
+2002.,
+3003.,
+3432.,
+3003.,
+2002.,
+1001.,
+364.,
+91.,
+14.,
+1.
+
+);
+
+const int kernel_radius = 7;
+#define KERNEL_SCALE_VALUE 1./(16384.)
+
