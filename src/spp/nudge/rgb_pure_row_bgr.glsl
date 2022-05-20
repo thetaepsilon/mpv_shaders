71722b613162ef69d7dcd6cf5aef4246f758c057
thetaepsilon <thetaepsilon@noreply.users.github.com>
2022-05-20 15:51:50 +0100

    [+] spp/nudge: reverse order rgb (bgr) subpixel row

---
+++ src/spp/nudge/rgb_pure_row_bgr.glsl
@@ -0,0 +1,13 @@
+//!WIDTH ${in}.w 3 *
+//!HEIGHT ${in}.h
+#define HPIXSZ 3
+#define VPIXSZ 1
+
+#define USE_MODULATION_LUT 1
+
+const vec3 modulate_lut[] = vec3[](
+	vec3(	0.,	0.,	1.),
+	vec3(	0.,	1.,	0.),
+	vec3(	1.,	0.,	0.)
+);
+
