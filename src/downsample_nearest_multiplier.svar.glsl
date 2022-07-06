087ab886c357cd43d6c3995f80d045c88b1a463b
thetaepsilon <thetaepsilon@noreply.users.github.com>
2022-07-06 17:04:59 +0100

    [*] downsample_nearest_multiplier.svar.glsl: nearest pixel calculation re-write (behaviour for non-integer scales affected)

--- src/downsample_nearest_multiplier.svar.glsl
+++ src/downsample_nearest_multiplier.svar.glsl
@@ -14,11 +14,11 @@
 vec2 offset = vec2(float(${ox:0}), float(${oy:0}));
 
 vec4 hook() {
-	vec2 inpix = gl_FragCoord.xy;
-	float tx = floor(inpix.x * float(SX));
-	float ty = floor(inpix.y * float(SY));
+	vec2 inpix = floor(gl_FragCoord.xy);
+	vec2 t = inpix * vec2(SX, SY);
+	vec2 nearest = floor(t);
 
-	vec2 pix = vec2(tx + 0.5, ty + 0.5) + offset;
+	vec2 pix = nearest + vec2(0.5) + offset;
 
 	vec2 pt = pix / SZ;
 	return TEXF(pt);
