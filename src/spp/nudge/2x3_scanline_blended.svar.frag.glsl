6b7b4ca4cc908a55b58b2aa3452e5b3cd3d468b5
thetaepsilon <thetaepsilon@noreply.users.github.com>
2022-07-10 21:45:34 +0100

    [+] spp/nudge: 2x3 scanline variant

---
+++ src/spp/nudge/2x3_scanline_blended.svar.frag.glsl
@@ -0,0 +1,28 @@
+//!WIDTH ${in}.w 2 *
+//!HEIGHT ${in}.h 3 *
+#define HPIXSZ 2
+#define VPIXSZ 3
+
+
+#define USE_DRIFT_LUT 1
+#define DRIFT_LUT_IS_ABS 1
+#define ROW(Y)	vec2(0, Y), vec2(0.5, Y)
+const vec2 drift_lut[2*3] = vec2[](
+	ROW(-1./256.),
+	ROW( 1./256.),
+	ROW(0.5)
+);
+#undef ROW
+#undef T
+
+
+
+#define USE_MODULATION_LUT 1
+const vec3 _ns_W = vec3(1.00);
+const vec3 _ns_D = vec3(${scanline_rowdim:1./64.});
+const vec3 modulate_lut[2*3] = vec3[](
+	_ns_W, _ns_W,
+	_ns_W, _ns_W,
+	_ns_D, _ns_D
+);
+
