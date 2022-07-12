96fc490151e2769cc54e6d966ec8221f1b8f4aa1
thetaepsilon <thetaepsilon@noreply.users.github.com>
2022-07-12 18:29:47 +0100

    [+] spp/blur_kernels: condensed binomials

---
+++ src/spp/blur_kernels/separable/binomial/condensed/binomial_3x_from_21x.frag.glsl
@@ -0,0 +1,11 @@
+const float kernel_data[] = float[](
+
+60460.,
+927656.,
+60460.
+
+);
+
+const int kernel_radius = 1;
+#define KERNEL_SCALE_VALUE pow(2., -20.)
+
---
+++ src/spp/blur_kernels/separable/binomial/condensed/binomial_3x_from_27x.frag.glsl
@@ -0,0 +1,11 @@
+const float kernel_data[] = float[](
+
+2533987.,
+62040890.,
+2533987.
+
+);
+
+const int kernel_radius = 1;
+#define KERNEL_SCALE_VALUE pow(2., -26.)
+
