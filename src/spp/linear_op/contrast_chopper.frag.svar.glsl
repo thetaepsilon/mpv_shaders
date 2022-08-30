d56337235f5d54fd1484300a2502d051455f9ea3
thetaepsilon <thetaepsilon@noreply.users.github.com>
2022-08-30 11:51:15 +0100

    [*] contrast_chopper: algorithm re-write

--- src/spp/linear_op/contrast_chopper.frag.svar.glsl
+++ src/spp/linear_op/contrast_chopper.frag.svar.glsl
@@ -1,7 +1,7 @@
-const float scaler = float(${n}) / (float(${n}) - 1.);
-
 vec3 f(vec3 col) {
-	#define F(c) ( (( col.c - 1. ) * scaler) + 1. )
-	return vec3( F(r), F(g), F(b) );
-	#undef F
+	const float minlight = ${minlight};
+	const float scaler = 1. / (1. - minlight);
+
+	vec3 chopped = (col - vec3(minlight)) * scaler;
+	return max(chopped, vec3(0.));
 };
