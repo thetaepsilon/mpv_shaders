7daa6187da310fc36952e1defe51b2a406447917
thetaepsilon <thetaepsilon@noreply.users.github.com>
2023-07-07 12:14:50 +0100

    [*] luminance_bloom.svar.glsl: overhaul algorithm and add circular cutoff to avoid box-like artefacts

--- src/luminance_bloom.svar.glsl
+++ src/luminance_bloom.svar.glsl
@@ -8,52 +8,71 @@
 
 #define TEXF ${in}_tex
 #define SZ ${in}_size
-const int radius = ${radius};
-const float exposure = ${e:1.0};
-const float bias = ${bias:1.0};
-const float base_mult = ${bm:1.0};
 
-#define FVEC3(v) (vec3((F(v.r)), (F(v.g)), (F(v.b))))
+const int radius = ${radius};
+const float radius_f = float(radius);
+
+const float dm = float(${distance_scale:1.});
+const float dm2 = dm * dm;
+
+const float d2limit = radius_f * radius_f;
+
+// prevents divide by zeroes, has the effect of making sources end up more diffused if increased.
+const float bias = ${bias:0.001};
+
+
+
+float distance_squared(vec2 p) {
+	// light falloff is 1 / d^2,
+	// however d (distance) itself is sqrt(x^2 + y^2),
+	// so the square and square root cancel out;
+	// therefore we can work in squared distance and drop the sqrt.
+	// thus we just want (x^2 + y^2).
+
+	vec2 p2 = p * p;	// vec2(x * x, y * y)
+
+	float d2 = p2.x + p2.y;
+	return d2;
+}
+
+
 
 vec4 hook() {
 	vec2 inpix = gl_FragCoord.xy;
-	vec3 reference = TEXF(${in}_pos).rgb;
 
-	// note: input assumed rgb only, no alpha channel!
-	//vec3 accum = reference;
-	vec3 accum = vec3(base_mult * reference);
-	vec3 total = vec3(base_mult);
+//#optreplace #if ${load_reference}
+//#optreplace vec3 reference = TEXF(${in}_pos).rgb;	// load_reference = ${load_reference}
+//#optreplace #endif	// load_reference = ${load_reference}
 
-	// vv inb4 coding style triggered
+	vec3 accum = vec3(0.);
+	float weight_total = 0.;
+
 	for (int i = -radius; i <= radius; i++) {
-	 for (int j = -radius; j <= radius; j++) {
-		vec2 target = inpix + vec2(float(i), float(j));
+	  for (int j = -radius; j <= radius; j++) {
+		vec2 offset = vec2(float(i), float(j));
+		vec2 target = inpix + offset;
 
-		// light falloff is 1 / d^2,
-		// however d (distance) itself is sqrt(x^2 + y^2),
-		// so the square and square root cancel out.
-		float d2 = (i * i) + (j * j);
-		d2 *= ${dm};
-		//d2 = sqrt(d2);
-		vec3 weight = vec3(1 / (d2 + bias));
+		float d2 = distance_squared(offset);
+		bool inside = (d2 <= d2limit);
 
-		vec3 c = TEXF(target / SZ).rgb;
-		accum += weight * c;
-		total += weight;
-	 }
+		if (inside) {
+			// commutivity: (distance * dm) * (distance * dm)
+			// (and we need a (scaled) distance squared for inverse square falloff)
+			// we already have distance squared, so...
+			float scaled_d2 = d2 * dm2;
+			float weight = 1. / (scaled_d2 + bias);
+
+			vec3 c = TEXF(target / SZ).rgb;
+			accum += weight * c;
+			weight_total += weight;
+		}
+	  }
 	}
 
-	vec3 raw = accum / total;
-	vec3 res = raw * exposure;
+	vec3 result = accum / vec3(weight_total);
 
-	// debug A-B viewing mode
-	//bool ab = fract(frame / 2.0) < 0.5;
-	//if (ab) res = reference;
+//#optreplace result = ${output_transform};
 
-	#define F(x) (min(x, 1.))
-	vec3 clamped = FVEC3(res);
-	#undef F
-//#optreplace clamped = pow(clamped, vec3(1 / ${postgamma}));
-	return vec4(clamped, 1.);
+	return vec4(result, 0.);
 }
 
