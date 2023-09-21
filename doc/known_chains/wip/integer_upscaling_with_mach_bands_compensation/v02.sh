8616055e30e6c558fb9dea0fb81ecb9af41e87b3
thetaepsilon <thetaepsilon@noreply.users.github.com>
2023-09-21 16:23:46 +0100

    [+] known chains: integer upscaling with mach bands compensation: version 2, tuned some constants

---
+++ doc/known_chains/wip/integer_upscaling_with_mach_bands_compensation/v02.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+set -eu;
+
+exec mpvhk \
+	--linear-upscaling --linear-downscaling \
+	--glsl-shader=$(spp kernel spp/blur_kernels/2d/edgedetect/3x3_laplacian_with_diagonals.frag.glsl < blur_kernel_2d.spp.svar.glsl \
+		| svar \
+			after LINEAR \
+			in LINEAR \
+			load_origin 1 \
+			input_transform 'log2(max(data, vec3(0.)) + vec3(exp2(-120.)))' \
+			output_transform '((original - (result * exp2(-8.))) / 256.) + vec3(1.)' \
+		| cs) \
+	--glsl-shader=$(svar at LINEAR in LINEAR x 3 y 1 out scale < integer_upscale_exact.svar.glsl | cs) \
+	--glsl-shader=$(spp \
+			kernel spp/blur_kernels/1d/laplacian_1d.glsl \
+		< blur_kernel_1d.spp.svar.glsl \
+		| svar \
+			after LINEAR \
+			in scale \
+			xs 1. \
+			load_origin 1 \
+			input_transform '(data - vec3(1.)) * 256.' \
+			output_transform 'exp2(original + (result * exp2(-6.)))' \
+		| cs) \
+	--glsl-shader=$(svar in scale shift 1 < chroma_shift.svar.glsl | cs) \
+	--glsl-shader=$(spp lutdata spp/nudge/vstretch_x3.glsl < simple_nudge_and_colour.spp.glsl \
+		| svar at LINEAR in scale \
+		| cs) \
+	--glsl-shader=$(svar in scale < autocentre_fixedsize.svar.glsl | cs) \
+	"$@";
