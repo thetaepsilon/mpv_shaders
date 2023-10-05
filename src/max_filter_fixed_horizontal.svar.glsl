4c93e09e8bee86c1e82ee4bd8d172e8f85520845
thetaepsilon <thetaepsilon@noreply.users.github.com>
2023-10-05 21:38:05 +0100

    [+] max_filter_fixed_horizontal.svar.glsl: add option to invert operation to be min() instead

--- src/max_filter_fixed_horizontal.svar.glsl
+++ src/max_filter_fixed_horizontal.svar.glsl
@@ -9,6 +9,21 @@
 
 
 
+vec3 accumulate(vec3 m, vec3 data) {
+#if ${min_mode:0}
+	m = min(m, data);
+#else
+	m = max(m, data);
+#endif
+	return m;
+}
+
+#if ${min_mode:0}
+const float initial = 1.0;
+#else
+const float initial = 0.0;
+#endif
+
 
 
 const int radius = ${r:1};
@@ -16,12 +31,12 @@ const int radius = ${r:1};
 vec4 hook() {
 	vec2 origin = gl_FragCoord.xy;
 
-	vec3 m = vec3(0.);
+	vec3 m = vec3(initial);
 	for (int x = -radius; x <= radius; x++) {
 		vec2 srcpix = origin + vec2(float(x), 0.0);
 		vec2 srcpt = srcpix / SZ;
 		vec3 data = TEXF(srcpt).rgb;
-		m = max(m, data);
+		m = accumulate(m, data);
 	}
 	return vec4(m, 1.);
 }
